056079d6a54f2b10d21397da9e189630a5ceb3af
unais <unaiswadkar1@gmail.com>
2026-08-03 01:35:47 +0530

    firstcommit

--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "../redux/cartSlice.js";
+import cartReducer from "./cartSlice.js";
 
 export const store = configureStore({
   reducer: {
